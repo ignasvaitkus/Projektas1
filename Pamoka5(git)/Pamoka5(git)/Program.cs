e5d9f6fba2ff397d4857a591304bcb4c230ad8ed
ignasvait <vaitkusignas@gmail.com>
2019-12-12 19:50:33 +0200

    Parasau pirma funkcija

--- Pamoka5(git)/Pamoka5(git)/Program.cs
+++ Pamoka5(git)/Pamoka5(git)/Program.cs
@@ -6,7 +6,19 @@ namespace Pamoka5_git_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int skaicius1 = Convert.ToInt32(Console.ReadLine());
+            int skaicius2 = Convert.ToInt32(Console.ReadLine());
+            int skaicius3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Max(skaicius1, skaicius2, skaicius3));
+
+        }
+        static int Max(int a, int b, int c)
+        {
+            int max;
+            if (a > b && a > c) max = a;
+            else if (b > c) max = b;
+            else max = c;
+            return max;
         }
     }
 }
